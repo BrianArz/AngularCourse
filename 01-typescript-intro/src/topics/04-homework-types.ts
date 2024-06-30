9e21faba9b46eeac495ea161db1410a4e56728c1
Arz Dev <arz.dev@outlook.com>
2024-06-29 21:24:54 -0600

    [typescript-intro] + homework types

---
+++ 01-typescript-intro/src/topics/04-homework-types.ts
@@ -0,0 +1,44 @@
+
+// Version 1
+// interface SuperHero {
+//     name: string, 
+//     age: number,
+//     address: {
+//         calle: string,
+//         pais: string,
+//         ciudad: string,
+//     },
+//     showAddress: () => string;
+// }
+
+// Version 2
+interface Address {
+    street: string, 
+    country: string,
+    city: string
+}
+
+interface SuperHero {
+    name: string,
+    age: number,
+    address: Address;
+    showAddress: () => string;
+}
+
+const superHeroe: SuperHero = {
+    name: 'Spiderman',
+    age: 30,
+    address: {
+        street: 'Main St',
+        country: 'USA',
+        city: 'NY'
+    },
+    showAddress() {
+        return this.name + ', ' + this.address.city + ', ' + this.address.country;
+    }
+}
+
+const address = superHeroe.showAddress();
+console.log( address );
+
+export {};
